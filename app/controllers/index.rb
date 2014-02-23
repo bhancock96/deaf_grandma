824b9c25819e517ff226d7af7c3b3ceb7eb7e585
Bryan <bryan@Bryans-MacBook-Pro.local>
2014-02-23 11:34:05 -0600

    initial commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,25 +1,32 @@
 get '/' do
-  @grandma = params[:grandma]
   # Look in app/views/index.erb
+  @contacts = Contact.all
   erb :index
 end
 
-post '/grandma' do
-  
-  input = params[:user_input]
-
-    if input == input.upcase
-      @grandma = "NO, NOT SINCE 1983" 
-    else
-      @grandma = "HUH?! SPEAK UP SONNY!"
-    end
-  
-  erb :index
-
-  # save response
-  # check what kind of response eg. upcase, downcase
-  # return some kind of view
-
-  #"Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+get '/contact' do
+	contact = Contact.find(params[:id])
+	if request.xhr?
+		content_type :json
+		{id: "#{contact.id}", name: "#{contact.name}", address: "#{contact.address}",
+		city_state: "#{contact.city_state}", phone_number: "#{contact.phone_number}",
+		email: "#{contact.email}"}.to_json
+	end
 end
 
+post '/address' do
+	contact = Contact.find_or_create_by_id(params[:id])
+	contact.update_attributes(params)
+	if contact.save
+		redirect '/'
+	end
+end
+
+
+post '/delete' do
+	if request.xhr?
+		contact = Contact.find(params[:id])
+		contact.delete
+		redirect '/'
+	end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,46 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  
+  var blank_form = function(){
+  	$('#name').val('');
+  	$('#address').val('');
+  	$('#city').val('');
+  	$('#phone').val('');
+  	$('#email').val('');
+  }
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.new_contact').on('click', function(){
+  	$(this).addClass('selected');
+  	$('.contact').removeClass('selected');
+  	blank_form();
+  });
+
+  $('.contact').on('click', function(){
+  	$('.new_contact').removeClass('selected');
+  	$('.contact').removeClass('selected');
+  	$(this).addClass('selected');
+
+  	var id = $(this).data('id')
+  	$.get('/contact', {id: id}, function(response){
+  		$('#name').val('' + response.name + '');
+  		$('#address').val('' + response.address + '');
+  		$('#city').val('' + response.city_state + '');
+  		$('#phone').val('' + response.phone_number + '');
+  		$('#email').val('' + response.email + '');
+  		$('#contact_id').remove();
+  		$('#contact_form').prepend('<input type="hidden" name="id" id="contact_id" value=' + response.id + '>')
+  	})
+  })
+
+  $('#delete').on('click', function(e){
+  	e.preventDefault();
+  	var contact = $('.contact').closest('.address_list').find('.selected').data('id')
+  	if (contact){
+  		$.post('/delete', {id: contact}, function(){
+  			blank_form();
+  			location.reload();
+  		})
+  	}
+  })
+
+
 });
